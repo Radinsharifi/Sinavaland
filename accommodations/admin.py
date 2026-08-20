cfbdd503c88d15331de920e7c2af21896ef745e2
Mohammadreza-Mos-hafian <mreza.moshafian@gmail.com>
2026-08-20 14:05:18 +0330

    create accommodation image model and show all accommodation images

--- accommodations/admin.py
+++ accommodations/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
-from .models import Accommodation
+from .models import Accommodation, AccommodationImage
+
 
 @admin.register(Accommodation)
 class AccommodationAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'location', 'price_per_night', 'capacity', 'is_available', 'get_reservation_count']
+    list_display = ['title', 'category', 'location', 'price_per_night', 'capacity', 'is_available',
+                    'get_reservation_count']
     list_filter = ['category', 'is_available', 'created_at']
     search_fields = ['title', 'location', 'description']
     prepopulated_fields = {'slug': ('title',)}
     filter_horizontal = ['reserved_by']
-    
+
     def get_reservation_count(self, obj):
         return obj.reserved_by.count()
+
     get_reservation_count.short_description = 'تعداد رزرو'
+
+
+@admin.register(AccommodationImage)
+class AccommodationImageAdmin(admin.ModelAdmin):
+    list_display = ['accommodation', 'image', 'created_at']
+    list_filter = ['created_at']
